bc287405c60968c629044c850eb81132076fd31c
Louis Bettens <louis@bettens.info>
2020-04-08 21:57:53 +0200

    refactor

--- core/src/main/scala/coprocessing/data/vector.scala
+++ core/src/main/scala/coprocessing/data/vector.scala
@@ -9,6 +9,7 @@ type Vector3D = Vector
 object Vector3D {
   def apply(n0: Scalar, n1: Scalar, n2: Scalar, n3: Scalar): Vector3D =
     Vector(n0, n1, n2, n3)
+
   val Zero = Vector3D(0,0,0,0)
   val UnitX = Vector3D(1,0,0,0)
   val UnitY = Vector3D(0,1,0,0)
@@ -16,14 +17,19 @@ object Vector3D {
   val UnitW = Vector3D(0,0,0,1)
 }
 
-given Show[Vector3D] {
-  private val Format = "Vector3D(%+.5f, %+.5f, %+.5f, %+.5f)"
-  def show(v: Vector3D) =
-    String.format(Format, v(0), v(1), v(2), v(3)).nn
-}
-
-extension on (self: Vector3D) {
+extension Vector3DOps on (self: Vector3D) {
   def toArray: IArray[Scalar] = unwrapV(self)
 
   @annotation.infix def dot(other: Vector3D): Scalar = dotVV(self, other)
+
+  def _1 = toArray(0)
+  def _2 = toArray(1)
+  def _3 = toArray(2)
+  def _4 = toArray(3)
+}
+
+given Show[Vector3D] {
+  private val Format = "Vector3D(%+.5f, %+.5f, %+.5f, %+.5f)"
+  def show(self: Vector3D) =
+    String.format(Format, self._1, self._2, self._3, self._4).nn
 }
