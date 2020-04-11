ae5e07033009c7a1d55c89712fab60357d949d6f
Louis Bettens <louis@bettens.info>
2020-04-11 18:25:20 +0200

    More

--- core/src/main/scala/coprocessing/core/sketch_api.scala
+++ core/src/main/scala/coprocessing/core/sketch_api.scala
@@ -1,10 +1,20 @@
 package coprocessing.core
 
+import processing.core.PApplet
+
 trait Sketch {
-  val settings: SizeOps ?=> Unit
+  val settings: (LegacyOps, SizeOps) ?=> Unit
+  val setup: LegacyOps ?=> Unit
+  val draw: LegacyOps ?=> Unit
 }
 
 trait SizeOps {
     def size(width: Int, height: Int): Unit
 }
 def size(width: Int, height: Int)(using SizeOps) = summon[SizeOps].size(width, height)
+
+trait LegacyOps {
+    def legacy[A](f: PApplet ?=> A): A
+}
+
+def thePApplet(using pa: PApplet) = pa
