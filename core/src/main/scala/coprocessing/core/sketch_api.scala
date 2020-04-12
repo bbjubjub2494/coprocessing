a68c39222bda2e61843759f77d3fed140ae4c230
Louis Bettens <louis@bettens.info>
2020-04-12 16:29:45 +0200

    move main entry point to a new module

--- core/src/main/scala/coprocessing/core/sketch_api.scala
+++ core/src/main/scala/coprocessing/core/sketch_api.scala
@@ -9,16 +9,20 @@ trait Sketch {
 trait SizeOps {
     def size(width: Int, height: Int): Unit
 }
-def size(width: Int, height: Int)(using SizeOps) = summon[SizeOps].size(width, height)
+inline def size(width: Int, height: Int)(using SizeOps) =
+  summon[SizeOps].size(width, height)
 
 trait FrameRateOps {
     def frameRate: Float
     def frameRate(fps: Float): Unit
 }
-def frameRate(using FrameRateOps) = summon[FrameRateOps].frameRate
-def frameRate(fps: Float)(using FrameRateOps) = summon[FrameRateOps].frameRate(fps)
+inline def frameRate(using FrameRateOps) =
+  summon[FrameRateOps].frameRate
+inline def frameRate(fps: Float)(using FrameRateOps) =
+  summon[FrameRateOps].frameRate(fps)
 
 trait LegacyOps[C] {
     def legacy[A](f: C ?=> A): A
 }
-def legacy[C, A](f: C ?=> A)(using LegacyOps[C]): A = summon[LegacyOps[C]].legacy(f)
+inline def legacy[C, A](f: C ?=> A)(using LegacyOps[C]): A =
+  summon[LegacyOps[C]].legacy(f)
