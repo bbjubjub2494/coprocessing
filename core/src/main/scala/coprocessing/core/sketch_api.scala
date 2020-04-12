cbf278798ffde8df7b4f5c987ae5068445e4c418
Louis Bettens <louis@bettens.info>
2020-04-12 10:41:21 +0200

    Add frameRate operations

--- core/src/main/scala/coprocessing/core/sketch_api.scala
+++ core/src/main/scala/coprocessing/core/sketch_api.scala
@@ -4,8 +4,8 @@ import processing.core.PApplet
 
 trait Sketch {
   def settings(): (LegacyOps, SizeOps) ?=> Unit = ()
-  def setup(): LegacyOps ?=> Unit = ()
-  def draw(): LegacyOps ?=> Unit = ()
+  def setup(): (FrameRateOps, LegacyOps) ?=> Unit = ()
+  def draw(): (FrameRateOps, LegacyOps) ?=> Unit = ()
 }
 
 trait SizeOps {
@@ -17,5 +17,12 @@ trait LegacyOps {
     def legacy[A](f: PApplet ?=> A): A
 }
 def legacy[A](f: PApplet ?=> A)(using LegacyOps): A = summon[LegacyOps].legacy(f)
+
+trait FrameRateOps {
+    def frameRate: Float
+    def frameRate(fps: Float): Unit
+}
+def frameRate(using FrameRateOps) = summon[FrameRateOps].frameRate
+def frameRate(fps: Float)(using FrameRateOps) = summon[FrameRateOps].frameRate(fps)
 
 def thePApplet(using pa: PApplet) = pa
